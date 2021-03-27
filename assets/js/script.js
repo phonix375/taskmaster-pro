89bdb3ba7fe6a2df198dcd926754b8b49324576e
Alexy Kotliar <kotliar.alex@gmail.com>
2021-03-27 19:43:54 -0400

    add css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,7 +82,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -215,12 +215,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event) {
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash=drag');
   },
   over: function(event) {
+    $(event.target).addClass('dropover-active');
   },
   out: function(event) {
+    $(event.target).removeClass('dropover-active');
   },
   update: function(event) {
     var tempArr = [];
@@ -259,12 +265,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $('.bottom-trash').removeClass('bottom-trash-active');
   },
   over: function(event, ui) {
-    console.log("over");
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event, ui) {
-    console.log("out");
+    $('.bottom-trash').removeClass('bottom-trash-active');
   }
 });
 // create date piker element in the model
@@ -272,3 +279,8 @@ $('#modalDueDate').datepicker({
   minDate: 1
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
